51264d9ec4509be7acd336f751621f5d86f6252d
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-06 17:46:36 +0200

    🗑️  Remove unneeded user functionality

--- rss_scraper/users/api/views.py
+++ rss_scraper/users/api/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -10,14 +9,9 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    lookup_field = "username"
-
-    def get_queryset(self, *args, **kwargs):
-        assert isinstance(self.request.user.id, int)
-        return self.queryset.filter(id=self.request.user.id)
 
     @action(detail=False)
     def me(self, request):
