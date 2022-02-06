47e95bce48177f0f331549738fd02dc47310700e
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-07 00:30:39 +0200

    ✨ Add dynamic model serializer to serialize & deserialize feed items objects

--- rss_scraper/feeds/api/serializers.py
+++ rss_scraper/feeds/api/serializers.py
@@ -2,7 +2,8 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from rss_scraper.feeds.models import Feed
+from rss_scraper.feeds.mixins import DynamicFieldsModelSerializer
+from rss_scraper.feeds.models import Feed, Item
 
 
 class FeedModelSerializer(serializers.ModelSerializer):
@@ -35,3 +36,26 @@ class FeedModelSerializer(serializers.ModelSerializer):
                 message=_("You've already registered a feed with this url before."),
             )
         ]
+
+
+class ItemDynamicFieldsModelSerializer(DynamicFieldsModelSerializer):
+    """
+    Dynamic model serializer to serialize and deserialize feed item instances with option
+        to only include and exclude specific fields.
+    """
+
+    feed = FeedModelSerializer()
+
+    class Meta:
+        model = Item
+        fields = (
+            "id",
+            "title",
+            "description",
+            "status",
+            "published_at",
+            "updated_at",
+            "created_at",
+            "feed",
+        )
+        read_only_fields = fields
