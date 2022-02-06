d70604797cc7fa2a2bca8287a9776d57b54b254e
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-06 18:49:04 +0200

    ✅ Add test cases for disable admin add & delete permissions mixins

--- rss_scraper/feeds/tests/test_admin.py
+++ rss_scraper/feeds/tests/test_admin.py
@@ -2,6 +2,10 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from rss_scraper.feeds.mixins import (
+    DisableAdminAddPermission,
+    DisableAdminDeletePermission,
+)
 from rss_scraper.feeds.models import Feed, Item
 
 pytestmark = pytest.mark.django_db
@@ -57,3 +61,24 @@ class TestAdminSiteForItemModel:
         response = admin_client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
+
+    def test__has_add_permission__with_disable_admin_add_permission_mixin__should_return_false(
+        self, admin_client
+    ):
+        class ModelAdminWithoutAddPermission(DisableAdminAddPermission):
+            pass
+
+        assert (
+            ModelAdminWithoutAddPermission().has_add_permission(admin_client) is False
+        )
+
+    def test__has_delete_permission__with_disable_admin_delete_permission_mixin__should_return_false(
+        self, admin_client
+    ):
+        class ModelAdminWithoutDeletePermission(DisableAdminDeletePermission):
+            pass
+
+        assert (
+            ModelAdminWithoutDeletePermission().has_delete_permission(admin_client)
+            is False
+        )
