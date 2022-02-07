e760772d9fb96de4334b54803adaf5bcf94f73f9
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-07 15:13:59 +0200

    ✅ Update feed app serializers tests to catch validation errors using pytest

--- rss_scraper/feeds/tests/test_serializers.py
+++ rss_scraper/feeds/tests/test_serializers.py
@@ -1,5 +1,5 @@
 import pytest
-from rest_framework.exceptions import ErrorDetail
+from rest_framework import exceptions
 
 from rss_scraper.feeds.api.serializers import (
     FeedModelSerializer,
@@ -14,12 +14,10 @@ class TestFeedModelSerializer:
     def test__feed_serializer__given_valid_url__should_pass_validations(
         self, random_url: str, dummy_drf_request
     ):
-        # Act
         serializer = FeedModelSerializer(
             data={"url": random_url}, context={"request": dummy_drf_request}
         )
 
-        # Assert
         assert serializer.is_valid()
         assert set(serializer.validated_data) == {"url", "user"}
         assert set(serializer.fields.keys()) == {
@@ -43,11 +41,11 @@ class TestFeedModelSerializer:
             data={}, context={"request": dummy_drf_request}
         )
 
-        assert serializer.is_valid() is False
-        assert serializer.errors.keys() == {"url"}
-        assert serializer.errors["url"] == [
-            ErrorDetail(string="This field is required.", code="required")
-        ]
+        with pytest.raises(exceptions.ValidationError) as err_info:
+            serializer.is_valid(raise_exception=True)
+
+        assert err_info.type == exceptions.ValidationError
+        assert err_info.value.args[0]["url"] == ["This field is required."]
 
     def test__feed_serializer__duplicate_url_for_same_user__should_raise_validation_error(
         self, random_url: str, dummy_drf_request
@@ -64,16 +62,16 @@ class TestFeedModelSerializer:
         )
 
         # Assert
-        assert serializer_1.is_valid()
+        serializer_1_is_valid = serializer_1.is_valid()
+        assert serializer_1_is_valid
         serializer_1.save()
 
-        assert serializer_2.is_valid() is False
-        assert serializer_2.errors.keys() == {"non_field_errors"}
-        assert serializer_2.errors["non_field_errors"] == [
-            ErrorDetail(
-                string="You've already registered a feed with this url before.",
-                code="unique",
-            )
+        with pytest.raises(exceptions.ValidationError) as err_info:
+            serializer_2.is_valid(raise_exception=True)
+
+        assert err_info.type == exceptions.ValidationError
+        assert err_info.value.args[0]["non_field_errors"] == [
+            "You've already registered a feed with this url before."
         ]
 
 
@@ -81,10 +79,8 @@ class TestItemDynamicFieldsModelSerializer:
     def test__item_serializer__given_valid_item__should_return_expected_fields(
         self, item_instance: Item
     ):
-        # Act
         serializer = ItemDynamicFieldsModelSerializer(item_instance)
 
-        # Assert
         assert set(serializer.data.keys()) == {
             "id",
             "title",
@@ -99,10 +95,8 @@ class TestItemDynamicFieldsModelSerializer:
     def test__dynamic_item_serializer__using_exclude_field__should_not_return_at_expected_fields(
         self, item_instance: Item
     ):
-        # Act
         serializer = ItemDynamicFieldsModelSerializer(item_instance, exclude=("feed",))
 
-        # Assert
         assert set(serializer.data.keys()) == {
             "id",
             "title",
@@ -116,10 +110,8 @@ class TestItemDynamicFieldsModelSerializer:
     def test__dynamic_item_serializer___using_custom_fields__should_return_only_included_fields(
         self, item_instance: Item
     ):
-        # Act
         serializer = ItemDynamicFieldsModelSerializer(
             item_instance, fields=("id", "title")
         )
 
-        # Assert
         assert set(serializer.data.keys()) == {"id", "title"}
