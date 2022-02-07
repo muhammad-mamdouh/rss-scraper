640537931b1ff66fda53e6f400e8a19266c74b23
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-07 20:56:10 +0200

    ✨ Add specific enum to hold various feed parsing error codes

--- rss_scraper/feeds/enums.py
+++ rss_scraper/feeds/enums.py
@@ -1,5 +1,13 @@
+from enum import Enum
+
 from django.db import models
 
 
 class ItemStatus(models.IntegerChoices):
     NEW, READ = range(1, 3)
+
+
+class FeedParsingErrorCodes(Enum):
+    IS_GONE = 410
+    URL_CHANGED = 301
+    CONTENT_NOT_CHANGED = 304
