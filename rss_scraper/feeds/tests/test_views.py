e0c4dd65b828f486418fa27bab01f2bbcf7549fb
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-07 02:16:53 +0200

    ✅ Add test cases for feed viewset list api endpoint

---
+++ rss_scraper/feeds/tests/test_views.py
@@ -0,0 +1,56 @@
+import pytest
+from django.urls import reverse
+from model_bakery import baker
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from config.settings.base import REST_FRAMEWORK
+from rss_scraper.feeds.models import Feed
+from rss_scraper.users.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+class TestFeedViewSetV1:
+    def test__list_api__with_anonymous_user__should_return_403(
+        self, api_client: APIClient, user: User
+    ):
+        baker.make(Feed, user=user, _quantity=11)
+
+        response = api_client.get(reverse("api:feed-list"))
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert (
+            response.json()["detail"] == "Authentication credentials were not provided."
+        )
+
+    def test__list_api__with_authenticated_user__should_return_paginated_feeds_list(
+        self, api_client: APIClient, user: User
+    ):
+        # Arrange
+        baker.make(Feed, user=user, _quantity=11)
+        api_client.force_login(user)
+
+        # Act
+        response = api_client.get(reverse("api:feed-list"))
+
+        # Assert
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()["results"]) == REST_FRAMEWORK["PAGE_SIZE"]
+
+    def test__list_api__with_feeds_for_different_users__should_return_feeds_of_authenticated_user(
+        self, api_client: APIClient, user: User
+    ):
+        # Arrange
+        user_2 = baker.make(User)
+        user_1_feeds_count = 4
+        baker.make(Feed, user=user, _quantity=user_1_feeds_count)
+        baker.make(Feed, user=user_2, _quantity=5)
+        api_client.force_login(user)
+
+        # Act
+        response = api_client.get(reverse("api:feed-list"))
+
+        # Assert
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()["results"]) == user_1_feeds_count
