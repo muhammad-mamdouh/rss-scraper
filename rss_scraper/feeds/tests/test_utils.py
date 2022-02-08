1d1958dceb8d8c3956633a11dd28ae15bb2da827
Mohamed Mamdouh <mahammad.mamdouh@gmail.com>
2022-02-08 03:49:44 +0200

    ✅ Add test case for the send notification to user about stalled feed

--- rss_scraper/feeds/tests/test_utils.py
+++ rss_scraper/feeds/tests/test_utils.py
@@ -1,10 +1,19 @@
 import datetime
 import time
 
+import pytest
 import pytz
 from django.conf import settings
+from django.core import mail
+from django.utils.translation import gettext_lazy as _
 
-from rss_scraper.feeds.utils import get_datetime_from_struct_time
+from rss_scraper.feeds.models import Feed
+from rss_scraper.feeds.utils import (
+    get_datetime_from_struct_time,
+    notify_feed_creator_with_stalled_feed,
+)
+
+pytestmark = pytest.mark.django_db
 
 
 def test__get_datetime_from_struct_time__given_time_struct__should_have_valid_datetime(
@@ -15,3 +24,15 @@ def test__get_datetime_from_struct_time__given_time_struct__should_have_valid_da
     assert type(returned_datetime) is datetime.datetime
     assert returned_datetime.tzinfo == pytz.timezone(settings.TIME_ZONE)
     assert returned_datetime.year == time_struct.tm_year
+
+
+def test__notify_feed_creator_with_stalled_feed__with_valid_feed_instance__should_send_email_to_feed_creator(
+    feed_instance: Feed,
+):
+    notify_feed_creator_with_stalled_feed(feed_instance)
+
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].subject == _(
+        "[RSS Scraper] Sorry, One of your registered feeds has been stalled!"
+    )
+    assert mail.outbox[0].recipients() == [feed_instance.user.email]
